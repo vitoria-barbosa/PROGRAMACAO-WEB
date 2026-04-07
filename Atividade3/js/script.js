009097d61263f1247c7a7b8ee069479f45256eb9
vitoria <vitoriabf2006@gmail.com>
2026-04-06 23:32:29 -0300

    :sparkles:feat: adiciona atividade 3 e alterações na atividade 1(atividade2).

---
+++ Atividade3/js/script.js
@@ -0,0 +1,18 @@
+function mudarCorFundo() {
+  let inputCorFundo = document.getElementById("cor-fundo");
+
+  inputCorFundo.addEventListener("input", () => {
+    let cor = inputCorFundo.value;
+    document.body.style.backgroundImage = "none";
+    document.body.style.backgroundColor = cor;
+  });
+}
+
+function mudarCorTexto() {
+  let inputCorTexto = document.getElementById("cor-texto");
+
+  inputCorTexto.addEventListener("input", () => {
+    let cor = inputCorTexto.value;
+    document.body.style.color = cor;
+  });
+}
